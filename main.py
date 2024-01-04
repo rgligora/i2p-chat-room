054bc63f33dfd5d6111c39ceddcd75d6106f4115
rgligora <99548015+rgligora@users.noreply.github.com>
2024-01-04 10:56:44 +0100

    Fixed issue in main.py

--- main.py
+++ main.py
@@ -107,4 +107,4 @@ def disconnect():
 
 
 if __name__ == "__main__":
-    eventlet.wsgi.server(eventlet.listen(('127.0.0.1',5000)), app)
+    eventlet.wsgi.server(eventlet.listen(('0.0.0.0',5000)), app)
